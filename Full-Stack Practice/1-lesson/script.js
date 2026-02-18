d914d65f28d3e67841218bceb4a8abb65a5b4bcf
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-18 11:49:23 +0500

    new code

---
+++ Full-Stack Practice/1-lesson/script.js
@@ -0,0 +1,40 @@
+//Birinchi misol:
+
+// let a = Number(prompt("Kvadrat tomonini kriting: "));
+// let b = 4 * a;
+// console.log("Perimetri:", b);
+
+//Ikkinchi misol:
+// let a = Number(prompt("kubni kriting"));
+
+// let  = v * 3;
+// let p = 6 * (a ** 2);
+
+// alert(+v+p)
+
+// const a = Number(prompt("Kubning yon tomoni a ni kiriting:"));
+
+// const V = a ** 3;
+// const S = 6 * (a ** 2);
+
+// alert("V = " + V + "\nS = " + S);
+
+
+// 10 - misol
+// let a = Number(prompt("birinchi sondi kriting: "));
+// let b = Number(prompt("Ikkinchi sondi kriting: "));
+
+// let bir = a + b;
+// let ikki = a * b;
+// let uch = a * a ;
+// let tort = b * b;
+
+// console.log("yigindi" + bir + "\nko'paytma:"+ ikki + "\nkvadrati" + tort);
+
+let x = Number(prompt("Son kiriting:"));
+let modul;
+
+if (x < 0) modul = -x;
+else modul = x;
+
+console.log(modul);
