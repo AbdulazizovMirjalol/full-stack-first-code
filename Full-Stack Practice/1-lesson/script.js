0b6c875d99341b7b65b5836a1ed5901f022053e7
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-19 23:10:02 +0500

    vfkdbmvir

--- Full-Stack Practice/1-lesson/script.js
+++ Full-Stack Practice/1-lesson/script.js
@@ -1,10 +1,10 @@
-//Birinchi misol:
+// Birinchi misol:
 
 // let a = Number(prompt("Kvadrat tomonini kriting: "));
 // let b = 4 * a;
 // console.log("Perimetri:", b);
 
-//Ikkinchi misol:
+// Ikkinchi misol:
 // let a = Number(prompt("kubni kriting"));
 
 // let  = v * 3;
@@ -20,21 +20,21 @@
 // alert("V = " + V + "\nS = " + S);
 
 
-// 10 - misol
-// let a = Number(prompt("birinchi sondi kriting: "));
-// let b = Number(prompt("Ikkinchi sondi kriting: "));
+10 - misol
+let a = Number(prompt("birinchi sondi kriting: "));
+let b = Number(prompt("Ikkinchi sondi kriting: "));
 
-// let bir = a + b;
-// let ikki = a * b;
-// let uch = a * a ;
-// let tort = b * b;
+let bir = a + b;
+let ikki = a * b;
+let uch = a * a ;
+let tort = b * b;
 
-// console.log("yigindi" + bir + "\nko'paytma:"+ ikki + "\nkvadrati" + tort);
+console.log("yigindi" + bir + "\nko'paytma:"+ ikki + "\nkvadrati" + tort);
 
-let x = Number(prompt("Son kiriting:"));
-let modul;
+// let x = Number(prompt("Son kiriting:"));
+// let modul;
 
-if (x < 0) modul = -x;
-else modul = x;
+// if (x < 0) modul = -x;
+// else modul = x;
 
-console.log(modul);
+// console.log(modul);
