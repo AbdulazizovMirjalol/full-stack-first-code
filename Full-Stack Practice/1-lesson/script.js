8155e70bd2593b1bd9bac7365f0f2dd3ddf9f430
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-19 23:12:14 +0500

    FNDBNGDBF

--- Full-Stack Practice/1-lesson/script.js
+++ Full-Stack Practice/1-lesson/script.js
@@ -19,17 +19,16 @@
 
 // alert("V = " + V + "\nS = " + S);
 
-
-10 - misol
+10 - misol;
 let a = Number(prompt("birinchi sondi kriting: "));
 let b = Number(prompt("Ikkinchi sondi kriting: "));
 
 let bir = a + b;
 let ikki = a * b;
-let uch = a * a ;
+let uch = a * a;
 let tort = b * b;
 
-console.log("yigindi" + bir + "\nko'paytma:"+ ikki + "\nkvadrati" + tort);
+console.log("yigindi" + bir + "\nko'paytma:" + ikki + "\nkvadrati" + tort);
 
 // let x = Number(prompt("Son kiriting:"));
 // let modul;
