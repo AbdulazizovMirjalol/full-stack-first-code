ee910025035d81356a7b60735ae91465309566d4
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-21 22:00:56 +0500

    sdgbjdgjnbkdf

---
+++ Full-Stack Practice/3-lesson/script.js
@@ -0,0 +1,10 @@
+function add(a, b) { return a + b; }
+function sub(a, b) { return a - b; }
+function mul(a, b) { return a * b; }
+function div(a, b) {
+  if (b === 0) return "0 ga bo'lib bo'lmaydi";
+  return a / b;
+}
+
+console.log(add(10, 5)); // 15
+console.log(div(10, 0)); // "0 ga bo‘lib bo‘lmaydi"
