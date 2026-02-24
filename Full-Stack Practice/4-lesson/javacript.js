d7b162143a1b2c70abe49957f3477b485ba8bda7
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-24 09:51:12 +0500

    achdf

--- Full-Stack Practice/4-lesson/javacript.js
+++ Full-Stack Practice/4-lesson/javacript.js
@@ -8,21 +8,32 @@
 // let mirjalolName = "Mirjalol";
 // console.log(mirjalolName);
 
-let age = Number(prompt("Yoshingizni kriting: "));
+// let age = Number(prompt("Yoshingizni kriting: "));
 
-if (age >= 18){
-    console.log("Siz voyaga yetgansiz!")
+// if (age >= 18){
+//     console.log("Siz voyaga yetgansiz!")
+// }
+
+// else
+//     if (age >= 16 && age <= 18)
+//     {
+//         console.log("siz balog'atga yetgansiz!");
+//     }
+//     if (age >= 100)
+//     {
+//         console.log("Siz juda ulug' yoshdasiz! sizga yanada ko'p umiz tilayman!")
+//     }
+//     else{
+//         console.log("Siz balog'atga yetmagansiz!");
+//     }
+
+let a = 18, b = 20;
+
+if (a !== b){
+    console.log(a + b)
 }
-
-else
-    if (age >= 16 && age <= 18)
-    {
-        console.log("siz balog'atga yetgansiz!");
-    }
-    if (age >= 100)
-    {
-        console.log("Siz juda ulug' yoshdasiz! sizga yanada ko'p umiz tilayman!")
-    }
-    else{
-        console.log("Siz balog'atga yetmagansiz!");
-    }
+else{
+    a = 0 
+    b = 0
+    console.log(a,b)
+}
