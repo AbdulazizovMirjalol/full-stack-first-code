3e9e1c41093d45a360f877e40a71f18bdbab272d
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-24 08:59:39 +0500

    sdvnjsnfdv

--- Full-Stack Practice/4-lesson/javacript.js
+++ Full-Stack Practice/4-lesson/javacript.js
@@ -1,6 +1,9 @@
 // alert("Salom dunyo!");
-let ism  = "Mirjalol";
-let familya = "Abdulazizov";
-let yosh = 18;
-console.log(ism,familya);
-console.log(yosh);
+// let ism  = "Mirjalol";
+// let familya = "Abdulazizov";
+// let yosh = 18;
+// console.log(ism,familya);
+// console.log(yosh);
+
+let mirjalolName = "Mirjalol";
+console.log(mirjalolName);
