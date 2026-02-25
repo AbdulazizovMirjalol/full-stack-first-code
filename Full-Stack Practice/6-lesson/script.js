7bef044ce69a90ac04e7923f43b062835342afa1
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-25 11:50:57 +0500

    djvnfs

--- Full-Stack Practice/6-lesson/script.js
+++ Full-Stack Practice/6-lesson/script.js
@@ -27,7 +27,44 @@
 // let result = powerA(3);
 // console.log(result)
 
-function powerA234(a){
-    return console.log(`a ** 2 => ${a**2} a ** 3 => ${a ** 3} a ** 4 ${a ** 4}`)
+// function powerA234(a){
+//     return console.log(`a ** 2 => ${a**2} a ** 3 => ${a ** 3} a ** 4 ${a ** 4}`)
+// }
+// powerA234(5)
+
+// function Triangle(a) {
+//   // a — tomon uzunligi
+//   const P = 3 * a;
+//   const S = (a * a * Math.sqrt(3)) / 4;
+//   return { P, S };
+// }
+
+// // ---- Input ----
+// const input = fs.readFileSync(0, "utf8").trim();
+// const a = parseFloat(input);
+
+// console.log("Perimetri:", P.toFixed(4));
+// console.log("Yuzasi:", S.toFixed(4));
+
+// function Triangle(a){
+//     let p = 3 * a;
+//     let s = a + a ** 2;
+//     return {p,s}
+
+//     }
+// let natija = Triangle(5);
+// console.log(natija)
+
+function RectPS(x1, y1, x2, y2) {
+  let a = Math.abs(x2 - x1);
+  let b = Math.abs(y2 - y1);
+  let p = 2 * (a + b);
+  let s = a * b;
+  return { p, s };
 }
-powerA234(5)
+
+let r1 = RectPS(1, 2, 6, 8);
+let r2 = RectPS(-3, 1, 2, 5);
+
+console.log("1-rectangle:", r1);
+console.log("2-rectangle:", r2);
