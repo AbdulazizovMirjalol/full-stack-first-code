542561741b163e1a323e221f2ea20146bf13c0e6
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-25 11:13:06 +0500

    arrow function

--- Full-Stack Practice/6-lesson/script.js
+++ Full-Stack Practice/6-lesson/script.js
@@ -8,9 +8,15 @@
 // }
 // console.log(sum(7, 8));
 
-function sum(m, r) {
-  return m + r;
-}
-let result = sum(10, 20);
-console.log(result);
-console.log(sum(10, 40));
+// function sum(m, r) {
+//   return m + r;
+// }
+// let result = sum(10, 20);
+// console.log(result);
+// console.log(sum(10, 40));
+
+const sum = (a, b) => {
+  return a * b;
+};
+let result = sum(8,8)
+console.log(result)
