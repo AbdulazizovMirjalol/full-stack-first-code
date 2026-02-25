9b82abb052d031fc3b9156b41545444913de15d5
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-02-25 11:10:50 +0500

    vfsknv

--- Full-Stack Practice/6-lesson/script.js
+++ Full-Stack Practice/6-lesson/script.js
@@ -1,15 +1,16 @@
-// function salom(){
-//     console.log("Salom Mirjalol");
+// function salom() {
+//   console.log("Salom Mirjalol");
 // }
 // salom();
 
-// function sum(a,b){
-//     return a * b;
+// function sum(a, b) {
+//   return a * b;
 // }
-// console.log(sum(7,8));
+// console.log(sum(7, 8));
 
-function sum (m,r){
-    return m+r;
+function sum(m, r) {
+  return m + r;
 }
-let result = sum (10,20);
-console.log(result)
+let result = sum(10, 20);
+console.log(result);
+console.log(sum(10, 40));
