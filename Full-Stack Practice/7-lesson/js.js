3411107e5c21dba3220fe2c742143a0ff51e2da1
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-03 09:40:16 +0500

    vcvcx

--- Full-Stack Practice/7-lesson/js.js
+++ Full-Stack Practice/7-lesson/js.js
@@ -1,22 +1,47 @@
-function salom (){
-    console.log("salom Mirjalol");
-}
-salom();
+// function salom (){
+//     console.log("salom Mirjalol");
+// }
+// salom();
 
-function qoshish (a, b){
-    return a + b;
-}
-console.log(qoshish(7,10));
+// function qoshish (a, b){
+//     return a + b;
+// }
+// console.log(qoshish(7,10));
 
-let ism = "Abdulazizov Mirjalol";
+// let ism = "Abdulazizov Mirjalol";
 
-function m(){
-    console.log(ism);
-}
-m();
+// function m(){
+//     console.log(ism);
+// }
+// m();
 
-function test(){
-    let yosh = 20;
-    console.log(yosh);
-}
-test();
+// function test(){
+//     let yosh = 20;
+//     console.log(yosh);
+// }
+// test();
+
+// let n = 10;
+
+// function start(){
+//     for(i = 1; i < 10; i++){
+//         console.log(start)
+//     }
+// }
+
+// for (i = 0; i < 5; i++){
+//     console.log(i)
+// }
+
+
+// mevalar = ["olma","nok","hurmo","uzum"]
+// // talabaMirjalol = 
+// //     m = "Mirjalol",
+// //     surname = "Abdulazizov",
+// //     age = 18
+
+// for (const m of mevalar) {
+//     console.log(m,talabaMirjalol);
+    
+// }
+
