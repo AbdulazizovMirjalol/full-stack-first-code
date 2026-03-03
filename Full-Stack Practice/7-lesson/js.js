a813ff9a88cfd91bfee75207763cf882bdc9405c
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-03 11:26:54 +0500

    v csvx dfc

--- Full-Stack Practice/7-lesson/js.js
+++ Full-Stack Practice/7-lesson/js.js
@@ -1,101 +1,111 @@
-// function salom (){
-//     console.log("salom Mirjalol");
-// }
-// salom();
+// // function salom (){
+// //     console.log("salom Mirjalol");
+// // }
+// // salom();
 
-// function qoshish (a, b){
-//     return a + b;
-// }
-// console.log(qoshish(7,10));
+// // function qoshish (a, b){
+// //     return a + b;
+// // }
+// // console.log(qoshish(7,10));
 
-// let ism = "Abdulazizov Mirjalol";
+// // let ism = "Abdulazizov Mirjalol";
 
-// function m(){
-//     console.log(ism);
-// }
-// m();
+// // function m(){
+// //     console.log(ism);
+// // }
+// // m();
 
-// function test(){
-//     let yosh = 20;
-//     console.log(yosh);
-// }
-// test();
+// // function test(){
+// //     let yosh = 20;
+// //     console.log(yosh);
+// // }
+// // test();
 
-// let n = 10;
+// // let n = 10;
 
-// function start(){
-//     for(i = 1; i < 10; i++){
-//         console.log(start)
+// // function start(){
+// //     for(i = 1; i < 10; i++){
+// //         console.log(start)
+// //     }
+// // }
+
+// // for (i = 0; i < 5; i++){
+// //     console.log(i)
+// // }
+
+// // mevalar = ["olma","nok","hurmo","uzum"]
+// // // talabaMirjalol =
+// // //     m = "Mirjalol",
+// // //     surname = "Abdulazizov",
+// // //     age = 18
+
+// // for (const m of mevalar) {
+// //     console.log(m,talabaMirjalol);
+
+// // }
+
+// let data = [
+//     {
+//         name: "Mirjalol",
+//         surname: "Abdulazizov",
+//         age: 18,
+//         isMarried: false
+//     },
+//     {
+//         name: "Javlon",
+//         surname: "Abduxaliqov",
+//         age: 18,
+//         isMarried: false
+//     },
+//     {
+//         name: "Abzalbek",
+//         surname: "Toshpo'latov",
+//         age: 27,
+//         isMarried: true
 //     }
-// }
+// ]
 
-// for (i = 0; i < 5; i++){
-//     console.log(i)
-// }
+// // for (const i of data) {
+// //     console.log(`ismi ${i.name}, familya ${i.surname}, yoshi ${i.age}, holati: ${i.isMarried ? "Uylangan" : "Uylanmagan"}`);
+// // }
 
-// mevalar = ["olma","nok","hurmo","uzum"]
-// // talabaMirjalol =
-// //     m = "Mirjalol",
-// //     surname = "Abdulazizov",
-// //     age = 18
+// // let str1 = "Axror";
+// // let str2 = 'Salom, O\'zbekiston';
+// // console.log(str2);
 
-// for (const m of mevalar) {
-//     console.log(m,talabaMirjalol);
+// // let str3 = `Salom ${str2}`;
+// // console.log(str3);
 
-// }
+// // let str4 = 123;
+// // let str5 = String(str4);
+// // console.log(typeof str5);
+// // let str6 = "Javascript";
+// // console.log(str6. length);
 
-let data = [
-    {
-        name: "Mirjalol",
-        surname: "Abdulazizov",
-        age: 18,
-        isMarried: false
-    },
-    {
-        name: "Javlon",
-        surname: "Abduxaliqov",
-        age: 18,
-        isMarried: false
-    },
-    {
-        name: "Abzalbek",
-        surname: "Toshpo'latov",
-        age: 27,
-        isMarried: true
-    }
-]
+// // let str7 = "Salom Dunyo";
+// // console.log(str7.indexOf("Dunyo"));
+// // console.log(str7.lastIndexOf("o"));
+// // console.log(str7.includes("Salom"));
+// // console.log(str7.startsWith("Dunyo"));
+// // console.log(str7.endsWith("yo"));
+// // console.log(str7.search("m"));
 
-for (const i of data) {
-    console.log(`ismi ${i.name}, familya ${i.surname}, yoshi ${i.age}, holati: ${i.isMarried ? "Uylangan" : "Uylanmagan"}`);
-}
+// // function fun1(a, b) {
+// //   let result = a.indexOf(b);
+// //   if (result === -1) {
+// //     return "Mavjud emas"
+// //   } else {
+// //     return result
+// //   }
+// // }
+// // console.log(fun1("Salon","al"));
 
-let str1 = "Axror";
-let str2 = 'Salom, O\'zbekiston';
-console.log(str2);
+let ism = "Mirjalol Abdulazizov";
 
-let str3 = `Salom ${str2}`;
-console.log(str3);
-
-let str4 = 123;
-let str5 = String(str4);
-console.log(typeof str5);
-let str6 = "Javascript";
-console.log(str6. length);
-
-let str7 = "Salom Dunyo";
-console.log(str7.indexOf("Dunyo"));
-console.log(str7.lastIndexOf("o"));
-console.log(str7.includes("Salom"));
-console.log(str7.startsWith("Dunyo"));
-console.log(str7.endsWith("yo"));
-console.log(str7.search("m"));
-
-function fun1(a, b) {
-  let result = a.indexOf(b);
-  if (result === -1) {
-    return "Mavjud emas"
-  } else {
-    return result
-  }
-}
-console.log(fun1("Salon","al"));
+console.log(ism.toUpperCase());
+console.log(ism.toLowerCase());
+console.log(ism.trim());
+console.log(ism.trimStart());
+console.log(ism.trimEnd());
+console.log(ism.replace("M", 7));
+console.log(ism.replaceAll("A", 1));
