7aba7e597fb9354b5932a557129deb17558956d9
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-04 11:00:43 +0500

    dmsvkd

---
+++ Full-Stack Practice/8 - lesson/js.js
@@ -0,0 +1,18 @@
+function greet (name,age){
+    let result = `Salom mening ismim ${name}, va yoshim ${age} da`;
+    return  result
+}
+console.log(greet("Mirjalol",19));
+
+function maxOFTWO(A,B){
+    if (A>B){
+        return A;
+    }else{
+        return B;
+    }
+}
+
+console.log(maxOFTWO(10,2));
+console.log(maxOFTWO(234,4643));
+
+
