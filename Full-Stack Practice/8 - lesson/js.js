459a38c1ee05bf426397fa344b5b236782cb7660
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-04 11:21:20 +0500

    sfsvsd

--- Full-Stack Practice/8 - lesson/js.js
+++ Full-Stack Practice/8 - lesson/js.js
@@ -7,12 +7,25 @@ console.log(greet("Mirjalol",19));
 function maxOFTWO(A,B){
     if (A>B){
         return A;
+    }else if(A == B){
+        return A * B;   
     }else{
         return B;
     }
 }
 
-console.log(maxOFTWO(10,2));
+console.log(maxOFTWO(10,10));
 console.log(maxOFTWO(234,4643));
+
+function sumTo(n){
+    let sum = 0
+    for (let i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumTo(10));
+
+
 
 
