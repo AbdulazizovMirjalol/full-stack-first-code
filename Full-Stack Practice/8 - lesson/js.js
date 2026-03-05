564c4f4b83a843637aad2af8b8214a3719e68193
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-05 13:02:25 +0500

    vn dfbv

--- Full-Stack Practice/8 - lesson/js.js
+++ Full-Stack Practice/8 - lesson/js.js
@@ -40,27 +40,35 @@
 // console.log(maxTwo(150,10));
 // console.log(maxTwo(10,50));
 
-function validate(email, password) {
-  if (typeof email !== "string" || typeof password !== "string") {
-    return { ok: false, message: "Email va parol matn (string) bo'lishi kerak." };
-  }
+// function validate(email, password) {
+//   if (typeof email !== "string" || typeof password !== "string") {
+//     return { ok: false, message: "Email va parol matn (string) bo'lishi kerak." };
+//   }
 
-  // Email ichida @ bo'lishi shart
-  if (!email.includes("@")) {
-    return { ok: false, message: "Emailda @ belgisi bo'lishi kerak." };
-  }
+//   // Email ichida @ bo'lishi shart
+//   if (!email.includes("@")) {
+//     return { ok: false, message: "Emailda @ belgisi bo'lishi kerak." };
+//   }
 
-  // Password uzunligi 8 dan kam bo'lmasin
-  if (password.length < 8) {
-    return { ok: false, message: "Parol kamida 8 ta belgidan iborat bo'lishi kerak." };
-  }
+//   // Password uzunligi 8 dan kam bo'lmasin
+//   if (password.length < 8) {
+//     return { ok: false, message: "Parol kamida 8 ta belgidan iborat bo'lishi kerak." };
+//   }
 
-  return { ok: true, message: "Hammasi to'g'ri " };
+//   return { ok: true, message: "Hammasi to'g'ri " };
+// }
+
+// // Misollar:
+// console.log(validate("testgmail.com", "12345678")); // email xato
+// console.log(validate("test@gmail.com", "1234"));    // parol xato
+// console.log(validate("test@gmail.com", "12345678"));// ok
+
+
+function two(n){
+  let sum = 0;
+  for (let i = 1; i <= n; i++){
+    sum += i;
+  }
+  return sum
 }
-
-// Misollar:
-console.log(validate("testgmail.com", "12345678")); // email xato
-console.log(validate("test@gmail.com", "1234"));    // parol xato
-console.log(validate("test@gmail.com", "12345678"));// ok
-
-
+console.log(two(10));
