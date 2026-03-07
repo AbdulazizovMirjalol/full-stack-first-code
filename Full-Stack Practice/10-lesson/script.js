8bdfe51c8c9dffefe834c95c8c24f4c1975e2c51
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-07 23:53:51 +0500

    vfbnjdf

--- Full-Stack Practice/10-lesson/script.js
+++ Full-Stack Practice/10-lesson/script.js
@@ -27,29 +27,29 @@
 // }
 // console.log(kunRaqami);
 
-let son = 5;
-switch (son) {
-  case 2:
-    console.log("2 karra jadvali");
-    for (let i = 1; i <= 10; i++) {
-      console.log(`2 x ${i} = ${2 * i}`);
-    }
-    break;
-    case 5:
-        console.log("5 karra jadvali");
-        for (let i = 1; i <= 10; i++){
-            console.log(`5x ${i} = ${5 * i}`);
+// let son = 5;
+// switch (son) {
+//   case 2:
+//     console.log("2 karra jadvali");
+//     for (let i = 1; i <= 10; i++) {
+//       console.log(`2 x ${i} = ${2 * i}`);
+//     }
+//     break;
+//     case 5:
+//         console.log("5 karra jadvali");
+//         for (let i = 1; i <= 10; i++){
+//             console.log(`5x ${i} = ${5 * i}`);
             
-        }
-        break;
-    case 9:
-        console.log("9 karra jadvali");
-        for (let i = i; i <=10; i++){
-            console.log(`9x ${i} = ${9 * i}`);
+//         }
+//         break;
+//     case 9:
+//         console.log("9 karra jadvali");
+//         for (let i = i; i <=10; i++){
+//             console.log(`9x ${i} = ${9 * i}`);
 
-        }
-        break;
-        default:
-            console.log("Faqat 2 , 5, 9 karra uchun misol yozilgan");
+//         }
+//         break;
+//         default:
+//             console.log("Faqat 2 , 5, 9 karra uchun misol yozilgan");
             
-}
+// }
