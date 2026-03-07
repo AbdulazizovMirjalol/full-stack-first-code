0b3b069447638f7621689b8ccadbc5ace2640ce7
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-07 23:52:07 +0500

    svskbjnd

--- Full-Stack Practice/10-lesson/script.js
+++ Full-Stack Practice/10-lesson/script.js
@@ -1,31 +1,55 @@
 let kunRaqami = 3;
 
 switch (kunRaqami) {
-    case 1:
-        console.log("Duyshanba");
-        break;
-    case 2:
-        console.log("Seyshanba");
-        break;
-    case 3:
-        console.log("Chorshanba");
-        break;
-    case 4:
-        console.log("Payshanba");
-        break;
-    case 5:
-        console.log("Juma");
-        break;
-    case 6:
-        console.log("Shanba");
-        break;
-    case 7:
-        console.log("Yakshanba");
-        break;
-    default:
-        console.log("Bunday hafta kuni yo'q");
-
+  case 1:
+    console.log("Duyshanba");
+    break;
+  case 2:
+    console.log("Seyshanba");
+    break;
+  case 3:
+    console.log("Chorshanba");
+    break;
+  case 4:
+    console.log("Payshanba");
+    break;
+  case 5:
+    console.log("Juma");
+    break;
+  case 6:
+    console.log("Shanba");
+    break;
+  case 7:
+    console.log("Yakshanba");
+    break;
+  default:
+    console.log("Bunday hafta kuni yo'q");
 }
 console.log(kunRaqami);
 
+// let son = 5;
+// switch (son) {
+//   case 2:
+//     console.log("2 karra jadvali");
+//     for (let i = 1; i <= 10; i++) {
+//       console.log(`2 x ${i} = ${2 * i}`);
+//     }
+//     break;
+//     case 5:
+//         console.log("5 karra jadvali");
+//         for (let i = 1; i <= 10; i++){
+//             console.log(`5x ${i} = ${5 * i}`);
+            
+//         }
+//         break;
+//     case 9:
+//         console.log("9 karra jadvali");
+//         for (let i = i; i <=10; i++){
+//             console.log(`9x ${i} = ${9 * i}`);
 
+//         }
+//         break;
+//         default:
+//             console.log("Faqat 2 , 5, 9 karra uchun misol yozilgan");
+            
+// }
