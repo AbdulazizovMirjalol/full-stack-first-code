c3dcf40e6388004e0b6c56bbd0f59cb262a646f4
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-08 20:34:38 +0500

    vfnbkd

--- Full-Stack Practice/11-lesson/script.js
+++ Full-Stack Practice/11-lesson/script.js
@@ -1,7 +1,11 @@
 let yosh = 20;
 
-if (yosh >= 18){
-    console.log("Siz voyaga yetgansiz!");
-}else{
-    console.log("Siz hali voyaga yetmagansiz!");
+if (yosh >= 18) {
+  console.log("Siz voyaga yetgansiz!");
+} else {
+  console.log("Siz hali voyaga yetmagansiz!");
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Son", i);
 }
