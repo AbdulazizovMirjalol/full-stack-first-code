58001ab95d061ae0327ca801e28fb4f2b8fa9bc1
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-08 21:07:50 +0500

    vsknvdf

--- Full-Stack Practice/11-lesson/script.js
+++ Full-Stack Practice/11-lesson/script.js
@@ -1,21 +1,21 @@
-// let yosh = 20;
+let yosh = 20;
 
-// if (yosh >= 18) {
-//   console.log("Siz voyaga yetgansiz!");
-// } else {
-//   console.log("Siz hali voyaga yetmagansiz!");
-// }
+if (yosh >= 18) {
+  console.log("Siz voyaga yetgansiz!");
+} else {
+  console.log("Siz hali voyaga yetmagansiz!");
+}
 
-// for (let i = 1; i <= 10; i++) {
-//   console.log("Son", i);
-// }
+for (let i = 1; i <= 10; i++) {
+  console.log("Son", i);
+}
 
-// let i = 1;
+let i = 1;
 
-// while (i <= 5) {
-//   console.log("While Son", i);
-//   i++;
-
+while (i <= 5) {
+  console.log("While Son", i);
+  i++;
+}
 
 let son = prompt("Son kriting: ");
 
