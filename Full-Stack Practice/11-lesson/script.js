eb4771139f31f50f82e1b96a55331413fdedbaa4
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-08 21:06:47 +0500

    ffndv

--- Full-Stack Practice/11-lesson/script.js
+++ Full-Stack Practice/11-lesson/script.js
@@ -1,26 +1,26 @@
-// // let yosh = 20;
+// let yosh = 20;
 
-// // if (yosh >= 18) {
-// //   console.log("Siz voyaga yetgansiz!");
-// // } else {
-// //   console.log("Siz hali voyaga yetmagansiz!");
-// // }
-
-// // for (let i = 1; i <= 10; i++) {
-// //   console.log("Son", i);
-// // }
-
-// // let i = 1;
-
-// // while (i <= 5) {
-// //   console.log("While Son", i);
-// //   i++;
-// }
-
-// let son = prompt("Son kriting: ");
-
-// if (son % 2 === 0) {
-//   console.log("Kiritilgan son Juft");
+// if (yosh >= 18) {
+//   console.log("Siz voyaga yetgansiz!");
 // } else {
-//   console.log("Kiritilgan son Toq");
+//   console.log("Siz hali voyaga yetmagansiz!");
 // }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("Son", i);
+// }
+
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("While Son", i);
+//   i++;
+}
+
+let son = prompt("Son kriting: ");
+
+if (son % 2 === 0) {
+  console.log("Kiritilgan son Juft");
+} else {
+  console.log("Kiritilgan son Toq");
+}
