a78cc60139aac971ed77312185851f2ced36d73e
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-09 20:53:57 +0500

    csmdmkv

--- Full-Stack Practice/8-lesson/js.js
+++ Full-Stack Practice/8-lesson/js.js
@@ -1,18 +1,18 @@
-// function greet (name,age){
-//     let result = `Salom mening ismim ${name}, va yoshim ${age} da`;
-//     return  result
-// }
-// console.log(greet("Mirjalol",19));
+function greet (name,age){
+    let result = `Salom mening ismim ${name}, va yoshim ${age} da`;
+    return  result
+}
+console.log(greet("Mirjalol",19));
 
-// function maxOFTWO(A,B){
-//     if (A>B){
-//         return A;
-//     }else if(A == B){
-//         return A * B;   
-//     }else{
-//         return B;
-//     }
-// }
+function maxOFTWO(A,B){
+    if (A>B){
+        return A;
+    }else if(A == B){
+        return A * B;   
+    }else{
+        return B;
+    }
+}
 
 // console.log(maxOFTWO(10,10));
 // console.log(maxOFTWO(234,4643));
