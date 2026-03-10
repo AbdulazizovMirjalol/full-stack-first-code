67b57bc81215a8ef6542b2b65ba25fb39768f223
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-10 11:23:34 +0500

    student scores

---
+++ Full-Stack Practice/12-lesson/script.js
@@ -0,0 +1,12 @@
+const students = [
+    { name: "Mirjalol", scores: [80, 90, 100] },
+    { name: "Bob", scores: [50, 60, 70] },
+    { name: "Charlie", scores: [30, 40, 20] }
+];
+
+students.forEach(student => {
+    const sum = student.scores.reduce((a, b) => a + b, 0);
+    const average = sum / student.scores.length;
+
+    console.log(student.name + " average score: " + average);
+});
