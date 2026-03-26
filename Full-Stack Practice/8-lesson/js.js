8af0dc9c12459159e86313002271c7ccbe1e6953
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-03-26 17:31:26 +0500

    vfcmdv

--- Full-Stack Practice/8-lesson/js.js
+++ Full-Stack Practice/8-lesson/js.js
@@ -1,44 +1,44 @@
-// function greet (name,age){
-//     let result = `Salom mening ismim ${name}, va yoshim ${age} da`;
-//     return  result
-// }
-// console.log(greet("Mirjalol",19));
+function greet (name,age){
+    let result = `Salom mening ismim ${name}, va yoshim ${age} da`;
+    return  result
+}
+console.log(greet("Mirjalol",19));
 
-// function maxOFTWO(A,B){
-//     if (A>B){
-//         return A;
-//     }else if(A == B){
-//         return A * B;   
-//     }else{
-//         return B;
-//     }
-// }
+function maxOFTWO(A,B){
+    if (A>B){
+        return A;
+    }else if(A == B){
+        return A * B;   
+    }else{
+        return B;
+    }
+}
 
-// console.log(maxOFTWO(10,10));
-// console.log(maxOFTWO(234,4643));
+console.log(maxOFTWO(10,10));
+console.log(maxOFTWO(234,4643));
 
-// function sumTo(n){
-//     let sum = 0
-//     for (let i = 1; i <= n; i++){
-//         sum += i;
-//     }
-//     return sum;
-// }
-// console.log(sumTo(10));
+function sumTo(n){
+    let sum = 0
+    for (let i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumTo(10));
 
 
-// function maxTwo(a,b){
-//     if (a > b){
-//         return a;
-//     }else if(a == b){
-//         return a * b;
-//     }else{
-//         return b;
-//     }
-// }
-// console.log(maxTwo(150,150));
-// console.log(maxTwo(150,10));
-// console.log(maxTwo(10,50));
+function maxTwo(a,b){
+    if (a > b){
+        return a;
+    }else if(a == b){
+        return a * b;
+    }else{
+        return b;
+    }
+}
+console.log(maxTwo(150,150));
+console.log(maxTwo(150,10));
+console.log(maxTwo(10,50));
 
 function validate(email, password) {
   if (typeof email !== "string" || typeof password !== "string") {
